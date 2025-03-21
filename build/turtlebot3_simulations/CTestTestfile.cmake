6210cbb5afdc47c2233b20864cc4851081558200
Ali Yigit Ozudogru <aliyigitozudogruu@gmail.com>
2025-03-21 10:43:42 +0300

    initial commit

---
+++ build/turtlebot3_simulations/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ali/Desktop/ros2_nav2_tb/src/turtlebot3_simulations
+# Build directory: /home/ali/Desktop/ros2_nav2_tb/build/turtlebot3_simulations
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/turtlebot3_simulations/colcon_command_prefix_build.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/ali/Desktop/ros2_nav2_tb/install/turtlebot3_fake_node/share/turtlebot3_fake_node/package.sh"
+. "/home/ali/Desktop/ros2_nav2_tb/install/turtlebot3_gazebo/share/turtlebot3_gazebo/package.sh"
